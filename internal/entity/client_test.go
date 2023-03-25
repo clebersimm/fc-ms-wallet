c1acb96682fad8dcbbaa8f1941d6c67171e425a3
Cleber Simm <clebersimm@gmail.com>
2023-03-25 10:28:52 -0300

    create account

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -19,3 +19,25 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, client)
 }
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("John Doe Update", "j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Update", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "Name is required")
+}
+
+func TestAddAcount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	err := client.AddAcount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
