b3c787c844e7bd98134bcf4b329ca38650858d9c
Cleber Simm <clebersimm@gmail.com>
2023-06-28 17:03:02 -0300

    handlers web service mysql communication

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -9,23 +9,34 @@ import (
 	"github.com/clebersimm/fc-ms-wallet/internal/usecase/create_account"
 	"github.com/clebersimm/fc-ms-wallet/internal/usecase/create_client"
 	"github.com/clebersimm/fc-ms-wallet/internal/usecase/create_transaction"
+	"github.com/clebersimm/fc-ms-wallet/internal/web"
+	"github.com/clebersimm/fc-ms-wallet/internal/web/webserver"
 	"github.com/clebersimm/fc-ms-wallet/pkg/events"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "127.0.0.1", "3306", "wallet"))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	eventDispatcher := events.NewEventDispatcher()
 	transactionCreatedEvent := event.NewTransactionCreated()
-	eventDispatcher.Register("TransactionCreated", handler)
+	//eventDispatcher.Register("TransactionCreated", handler)
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
 	transactionDb := database.NewTransactionDB(db)
 	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
 	createAccountUseCase := create_account.NewCreateAccountUse(accountDb, clientDb)
 	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(transactionDb, accountDb, eventDispatcher, transactionCreatedEvent)
+
+	webserver := webserver.NewWebServer(":3000")
+	clientHandler := web.NewWebClientHandler(*createClientUseCase)
+	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
+	transactionHandler := web.NewWebTransactionHandler(*createTransactionUseCase)
+	webserver.AddHandler("/clients", clientHandler.CreateClient)
+	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
+	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
+	webserver.Start()
 }
